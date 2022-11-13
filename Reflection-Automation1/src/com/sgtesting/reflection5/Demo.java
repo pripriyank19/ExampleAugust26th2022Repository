35b9018d87bbbdc1f6275b212432dc9e760d85f5
unknown <skpriyanka123@gmail.com>
2022-11-13 14:11:07 +0530

    Reflection

---
+++ Reflection-Automation1/src/com/sgtesting/reflection5/Demo.java
@@ -0,0 +1,12 @@
+package com.sgtesting.reflection5;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		add();
+	}
+	public static void add()
+	{
+		System.out.println("addd");
+	}
+}
---
+++ Reflection-Automation1/src/com/sgtesting/reflection5/RunnerScript.java
@@ -0,0 +1,54 @@
+package com.sgtesting.reflection5;
+
+import java.io.FileInputStream; 
+
+import java.lang.reflect.Method;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class RunnerScript {
+
+	public static void main(String[] args) {
+		executeMethods();
+
+	}
+	private static void executeMethods()
+	{
+		FileInputStream fin=null;
+		Workbook wb=null;
+		Sheet sh=null;
+		Row row=null;
+		Cell cell=null;
+		try
+		{
+			fin=new FileInputStream(".\\DataFile\\Workbook1.xlsx");
+			wb=new XSSFWorkbook(fin);
+			sh=wb.getSheet("Sheet1");
+			int rc=sh.getPhysicalNumberOfRows();
+			for(int r=1;r<rc;r++)
+			{
+				row=sh.getRow(r);
+				cell=row.getCell(0);
+				String methodname=cell.getStringCellValue();
+				
+				cell=row.getCell(1);
+				String pkgclsname=cell.getStringCellValue();
+				System.out.println(methodname+"--->"+pkgclsname);
+				Class clsObj=Class.forName(pkgclsname);
+				Object obj=clsObj.getConstructor().newInstance();
+				
+				Method method=obj.getClass().getMethod(methodname);
+				method.invoke(obj);
+			}
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
