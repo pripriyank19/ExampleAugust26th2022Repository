790d1413b3317d2be7d0d05d87b888cea37c03de
unknown <skpriyanka123@gmail.com>
2022-11-15 10:47:21 +0530

    InterfaceDemo

---
+++ Web-Automation/src/com/sgtesting/tests/InterfaceDemo.java
@@ -0,0 +1,34 @@
+package com.sgtesting.tests;
+interface University
+{
+	void showUniversityName(String name);
+}
+
+interface EnggCollege
+{
+	void showCollegeName(String name);
+}
+
+class SLNEngg implements University,EnggCollege
+{
+	public void showUniversityName(String name)
+	{
+		System.out.println("University Name :"+name);
+	}
+	
+	public void showCollegeName(String name)
+	{
+		System.out.println("Engineering College Name :"+name);
+	}
+}
+public class InterfaceDemo {
+	public static void main(String[] args) {
+		University obj=new SLNEngg();
+		obj.showUniversityName("VTU Belguam");
+		
+		EnggCollege obj1=(EnggCollege) obj;
+		obj1.showCollegeName("SLN Engineering College");
+
+	}
+
+}
