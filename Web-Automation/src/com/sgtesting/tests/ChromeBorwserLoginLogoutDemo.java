6b5dad9ecac6289abda30b6becf588c7fa92ece4
unknown <skpriyanka123@gmail.com>
2022-11-15 10:40:33 +0530

    ChromeBorwserLoginLogoutDemo

---
+++ Web-Automation/src/com/sgtesting/tests/ChromeBorwserLoginLogoutDemo.java
@@ -0,0 +1,91 @@
+package com.sgtesting.tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBorwserLoginLogoutDemo {
+	public static WebDriver oBrowser=null;
+	public static void main(String[] args) {
+		launchBrowser();
+		navigate();
+		login();
+		minimizeFlyOutwindow();
+		logout();
+		closeApp();
+	}
+	
+	private static void launchBrowser()
+	{
+		try
+		{
+			String path=System.getProperty("user.dir");
+			System.setProperty("webdriver.chrome.driver", path+"\\Library\\drivers\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void navigate()
+	{
+		try
+		{
+			oBrowser.get("http://localhost/login.do");
+			Thread.sleep(5000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void login()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys("admin");
+			oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+			oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+			Thread.sleep(5000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void minimizeFlyOutwindow()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void logout()
+	{
+		try
+		{
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private static void closeApp()
+	{
+		try
+		{
+			oBrowser.quit();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
